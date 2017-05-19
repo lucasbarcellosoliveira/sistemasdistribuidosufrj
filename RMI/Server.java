4870e0b419190d25ab534d20b85051fefbb01507
Lucas Barcellos <lucas.barcellos.oliveira@poli.ufrj.br>
2017-05-19 18:00:53 -0300

    Update Server.java

--- RMI/Server.java
+++ RMI/Server.java
@@ -8,10 +8,10 @@ import java.rmi.registry.LocateRegistry;
 public class Server {
     public static void main(String args[]) throws RemoteException, AlreadyBoundException, MalformedURLException{
         System.out.println("Starting server...");
-        System.setProperty("java.rmi.server.hostname", "192.168.1.18");
+        System.setProperty("java.rmi.server.hostname", "127.0.0.1");
         LocateRegistry.createRegistry(5000);
         Op op=(Op) new OpImplementation();
-        Naming.bind("rmi://192.168.1.18:5000/VectorOperations", (Remote) op);
+        Naming.bind("rmi://127.0.0.1:5000/VectorOperations", (Remote) op);
         System.out.println("Server started!");
     }
 }
