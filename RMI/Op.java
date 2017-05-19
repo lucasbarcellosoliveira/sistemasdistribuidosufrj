c4bd36269f8765ef1114660f18c1541da4c49fad
Lucas Barcellos <lucas.barcellos.oliveira@poli.ufrj.br>
2017-05-19 18:38:05 -0300

    Update Op.java

--- RMI/Op.java
+++ RMI/Op.java
@@ -2,10 +2,10 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Op extends Remote{
-    public float[] logb (float[] a, float b) throws RemoteException;
-    public float[] power (float[] a, float b) throws RemoteException;
-    public float[] sine (float[] a) throws RemoteException;
-    public float sum (float[] a) throws RemoteException;
-    public float largerthan (float[] a, float b) throws RemoteException;
-    public float prod (float[] a) throws RemoteException;
+    public byte[] logb (byte[] a, byte b) throws RemoteException;
+    public byte[] power (byte[] a, byte b) throws RemoteException;
+    public byte[] sine (byte[] a) throws RemoteException;
+    public byte sum (byte[] a) throws RemoteException;
+    public byte largerthan (byte[] a, byte b) throws RemoteException;
+    public byte prod (byte[] a) throws RemoteException;
 }
