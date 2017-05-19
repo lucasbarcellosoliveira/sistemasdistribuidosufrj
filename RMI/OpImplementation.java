5f9240873cac73d000ad3214235975474c6f9888
Lucas Barcellos <lucas.barcellos.oliveira@poli.ufrj.br>
2017-05-19 18:39:03 -0300

    Update OpImplementation.java

--- RMI/OpImplementation.java
+++ RMI/OpImplementation.java
@@ -6,36 +6,36 @@ public class OpImplementation extends UnicastRemoteObject implements Op{
     OpImplementation() throws RemoteException{}
 
     @Override
-    public float[] logb (float[] a, float b) throws RemoteException{
+    public byte[] logb(byte[] a, byte b) throws RemoteException{
         //System.out.println("Operation logb called for array "+Arrays.toString(a)+" and base "+b);
         for (int i=0;i<a.length;i++)
-            a[i]=(float) (Math.log(a[i])/Math.log(b));
+            a[i]=(byte) (Math.log(a[i])/Math.log(b));
         //System.out.println("Operation completed. Returning array "+Arrays.toString(a));
         return a;
     }
 
     @Override
-    public float[] power(float[] a, float b) throws RemoteException {
+    public byte[] power(byte[] a, byte b) throws RemoteException {
         //System.out.println("Operation power called for array "+Arrays.toString(a)+" and expoent "+b);
         for (int i=0;i<a.length;i++)
-            a[i]=(float) Math.pow(a[i],b);
+            a[i]=(byte) Math.pow(a[i],b);
         //System.out.println("Operation completed. Returning array "+Arrays.toString(a));
         return a;
     }
 
     @Override
-    public float[] sine(float[] a) throws RemoteException {
+    public byte[] sine(byte[] a) throws RemoteException {
         //System.out.println("Operation sine called for array "+Arrays.toString(a));
         for (int i=0;i<a.length;i++)
-            a[i]=(float) Math.sin(a[i]);
+            a[i]=(byte) Math.sin(a[i]);
         //System.out.println("Operation completed. Returning array "+Arrays.toString(a));
         return a;
     }
 
     @Override
-    public float sum(float[] a) throws RemoteException {
+    public byte sum(byte[] a) throws RemoteException {
         //System.out.println("Operation sum called for array "+Arrays.toString(a));
-        float ret=0;
+        byte ret=0;
         for (int i=0;i<a.length;i++)
             ret+=a[i];
         //System.out.println("Operation completed. Returning value "+ret);
@@ -43,9 +43,9 @@ public class OpImplementation extends UnicastRemoteObject implements Op{
     }
 
     @Override
-    public float largerthan(float[] a, float b) throws RemoteException {
+    public byte largerthan(byte[] a, byte b) throws RemoteException {
         //System.out.println("Operation largerthan called for array "+Arrays.toString(a)+" and value "+b);
-        float ret=0;
+        byte ret=0;
         for (int i=0;i<a.length;i++)
             if (a[i]>b)
                 ret+=a[i];
@@ -54,9 +54,9 @@ public class OpImplementation extends UnicastRemoteObject implements Op{
     }
 
     @Override
-    public float prod(float[] a) throws RemoteException {
+    public byte prod(byte[] a) throws RemoteException {
         //System.out.println("Operation prod called for array "+Arrays.toString(a));
-        float ret=1;
+        byte ret=1;
         for (int i=0;i<a.length;i++)
             ret*=a[i];
         //System.out.println("Operation completed. Returning value "+ret);
