4033cd4012ce11dec3e1fb3b9b18b371dcdfae08
jvdavim <joaovictor_rj@poli.ufr.br>
2017-05-19 00:51:39 -0300

    implement all functions with multithreading

--- RPC/client.c
+++ RPC/client.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <pthread.h>
 
 #include <xmlrpc-c/base.h>
 #include <xmlrpc-c/client.h>
@@ -9,9 +10,20 @@
 #define VERSION "1.0"
 
 #define N 10
+#define K 1
+
+//=============================================================================//
+struct args_t{
+    xmlrpc_env env;
+    xmlrpc_value * result;
+    xmlrpc_value * myarray;
+    char * serverUrl;
+    char * methodName;
+};
+
 //=============================================================================//
 
-static void 
+static void
 dieIfFaultOccurred (xmlrpc_env * const envP) {
     if (envP->fault_occurred) {
         fprintf(stderr, "ERROR: %s (%d)\n",
@@ -27,20 +39,35 @@ double fRand(double fMin, double fMax)
 }
 //=============================================================================//
 
+void *
+rpcThread(void* arg){
+    struct args_t *args = (struct args_t *)malloc(sizeof(struct args_t));
+    args = arg;
+
+    /* Make the remote procedure call of first type functions */
+    // result = xmlrpc_client_call(&env, serverUrl, methodName,
+    //                              "(Ad)", (xmlrpc_value*) myarray, (xmlrpc_double) 2);
+
+    /* Make the remote procedure call of second type functions */
+    args->result = xmlrpc_client_call(&args->env, args->serverUrl, args->methodName,
+                                  "(A)", (xmlrpc_value*) args->myarray);//, (xmlrpc_double) 50);
+}
+//=============================================================================//
+
 int 
 main(double           const argc, 
      const char ** const argv) {
 
+    pthread_t threads[K];
     xmlrpc_env env;
-    xmlrpc_value * result;
-    xmlrpc_value * myarray;
     xmlrpc_value * item;
     xmlrpc_value * log;
-    xmlrpc_double var;
+//    xmlrpc_double var;
     double number;
     srand(time(NULL));
-    const char * const serverUrl = "http://localhost:8080/RPC2";
-    const char * const methodName = "prodx";
+    char * serverUrl = "http://localhost:8080/RPC2";
+    char * methodName = "prodx";
+    struct args_t args[K];
 
     if (argc-1 > 0) {
         fprintf(stderr, "This program has no arguments\n");
@@ -57,23 +84,37 @@ main(double           const argc,
     printf("Making XMLRPC call to server url '%s' method '%s' \n"
            , serverUrl, methodName);
 
-    /* Create random vector */
-    myarray = xmlrpc_array_new(&env);
-    
-    for (int i=0; i<N; i++){
-        number = fRand(0,100);
-        item = xmlrpc_double_new(&env, number);
-        xmlrpc_array_append_item(&env, myarray, item);
-        xmlrpc_DECREF(item);
+    /* Create random vector */    
+    for (int i=0; i<K; i++){
+        args[i].myarray = xmlrpc_array_new(&env);
+        for (int k=0; k<N/K; k++){
+            number = fRand(0,100);
+            item = xmlrpc_double_new(&env, number);
+            xmlrpc_array_append_item(&env, args[i].myarray, item);
+            xmlrpc_DECREF(item);
+        }
+        args[i].env = env;
+        args[i].serverUrl = serverUrl;
+        args[i].methodName = methodName;
     }
 
-    /* Make the remote procedure call of first type functions */
-    // result = xmlrpc_client_call(&env, serverUrl, methodName,
-    //                              "(Ad)", (xmlrpc_value*) myarray, (xmlrpc_double) 2);
 
-    /* Make the remote procedure call of second type functions */
-    result = xmlrpc_client_call(&env, serverUrl, methodName,
-                                  "(A)", (xmlrpc_value*) myarray);//, (xmlrpc_double) 50);
+//START CLOCK
+    for (int i=0; i<K; i++){
+        pthread_create(&threads[i], NULL, rpcThread, &args[i]);
+    }
+
+    for (int i=0; i<K; i++){
+        pthread_join(threads[i], NULL);
+    }
+//END CLOCK
+    // /* Make the remote procedure call of first type functions */
+    // // result = xmlrpc_client_call(&env, serverUrl, methodName,
+    // //                              "(Ad)", (xmlrpc_value*) myarray, (xmlrpc_double) 2);
+
+    // /* Make the remote procedure call of second type functions */
+    // result = xmlrpc_client_call(&env, serverUrl, methodName,
+    //                               "(A)", (xmlrpc_value*) myarray);//, (xmlrpc_double) 50);
 
     dieIfFaultOccurred(&env);
     
@@ -86,11 +127,12 @@ main(double           const argc,
     // }
 
     /* Get out second type functions */
-    xmlrpc_read_double(&env, result, &var);
-    printf("%F\n",var);
+    // xmlrpc_read_double(&env, result, &var);
+    // printf("%F\n",var);
 
     /* Dispose of our result value. */
-    xmlrpc_DECREF(result);
+    for (int i=0; i<K; i++)
+        xmlrpc_DECREF(args[i].result);
 
     /* Clean up our error-handling environment. */
     xmlrpc_env_clean(&env);
