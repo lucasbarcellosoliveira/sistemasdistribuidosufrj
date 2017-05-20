da8e12c42218dfe68f5b06b889e9e04175ceaada
Lucas Barcellos <lucas.barcellos.oliveira@poli.ufrj.br>
2017-05-20 14:44:57 -0300

    Update ProcessingThread.java

--- RMI/ProcessingThread.java
+++ RMI/ProcessingThread.java
@@ -3,11 +3,13 @@ public class ProcessingThread extends Thread{
     public byte b;
     public byte[] subarray;
     public Op threadOp;
+    public int ret;
 
     @Override
     public void run(){
         try {
             subarray=threadOp.logb(subarray, b);
+            //ret=threadOp.prod(subarray);
         }
         catch (Exception e) {
             e.printStackTrace();
