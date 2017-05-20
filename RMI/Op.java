868680ca7073834883ffc9c03fc2315ff3cf6574
Lucas Barcellos <lucas.barcellos.oliveira@poli.ufrj.br>
2017-05-20 14:44:02 -0300

    Update Op.java

--- RMI/Op.java
+++ RMI/Op.java
@@ -5,7 +5,7 @@ public interface Op extends Remote{
     public byte[] logb (byte[] a, byte b) throws RemoteException;
     public byte[] power (byte[] a, byte b) throws RemoteException;
     public byte[] sine (byte[] a) throws RemoteException;
-    public byte sum (byte[] a) throws RemoteException;
-    public byte largerthan (byte[] a, byte b) throws RemoteException;
-    public byte prod (byte[] a) throws RemoteException;
+    public int sum (byte[] a) throws RemoteException;
+    public int largerthan (byte[] a, byte b) throws RemoteException;
+    public int prod (byte[] a) throws RemoteException;
 }
